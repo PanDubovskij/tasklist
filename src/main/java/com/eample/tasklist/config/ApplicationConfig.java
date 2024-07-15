44022a17a42f8311820a6cc5f4d44264cc2b8482
SirDubovskij <SirDubovskij@gmail.com>
2024-07-15 10:44:59 +0300

    upgrade spring boot to 3.1.2

--- src/main/java/com/eample/tasklist/config/ApplicationConfig.java
+++ src/main/java/com/eample/tasklist/config/ApplicationConfig.java
@@ -21,6 +21,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -90,33 +91,43 @@ public class ApplicationConfig {
     public SecurityFilterChain filterChain(
             final HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf().disable()
-                .cors()
-                .and()
-                .httpBasic().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(((request, response, exception) -> {
-                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                    response.getWriter().write("Unauthorized.");
-                }))
-                .accessDeniedHandler(((request, response, exception) -> {
-                    response.setStatus(HttpStatus.FORBIDDEN.value());
-                    response.getWriter().write("Unauthorized.");
-                }))
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .anonymous().disable()
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(managment
+                        -> managment.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .exceptionHandling(
+                        configurer -> configurer.authenticationEntryPoint((
+                                        (request, response, exception) -> {
+                                            response.setStatus(
+                                                    HttpStatus
+                                                            .UNAUTHORIZED
+                                                            .value());
+                                            response.getWriter()
+                                                    .write("Unauthorized.");
+                                        }))
+                                .accessDeniedHandler((
+                                        (request, response, exception) -> {
+                                            response.setStatus(
+                                                    HttpStatus
+                                                            .FORBIDDEN
+                                                            .value());
+                                            response.getWriter()
+                                                    .write("Unauthorized.");
+                                        })))
+                .authorizeHttpRequests(
+                        configurer ->
+                                configurer.requestMatchers("/api/v1/auth/**")
+                                        .permitAll()
+                                        .requestMatchers("/swagger-ui/**")
+                                        .permitAll()
+                                        .requestMatchers("/v3/api-docs/**")
+                                        .permitAll()
+                                        .anyRequest().authenticated())
+                .anonymous(AbstractHttpConfigurer::disable)
                 .addFilterBefore(new JwtTokenFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class);
-
         return httpSecurity.build();
     }
 }
